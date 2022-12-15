e4a9874a745916ec09e8f67335ec0c126f27992c
Ingrid Radu <105636229+IngridRadu@users.noreply.github.com>
2022-12-15 17:43:41 +0200

    chain operations

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const backButton =document.querySelector(".backspace");
 const equalsButton = document.querySelector(".equals-key");
 const displayResult = document.querySelector(".display-result");
 const decimalButton = document.querySelector(".point");
-
-displayResult.textContent = "";
+let operatorActive = false;
 
 const add = (num1,num2) => {
   return num1+num2;
@@ -29,87 +28,132 @@ const operate = (operator, num1,num2) => {
     case "*" :
     return multiply(num1,num2);
     case "/" :
-    return divide(num1,num2);
+    return divide(num1,num2);  
   }
 }
 
-let storedNumber = "";
+let secondNumber = "";
 let firstNumber = "";
-let clickedOperator = "";
+let clickedOperator = null;
+let secondOperator = null;
 let result = "";
-displayResult.textContent = "";
+
+const calculate = (operator,num1,num2) => {
+  const result = operate(operator, parseFloat(num1), parseFloat(num2)).toFixed(5).replace(/[.,]00$/, "");
+  displayResult.textContent = parseFloat(result);
+  firstNumber = result;
+  console.log(parseFloat(result));
+  secondNumber = '';
+}
 
 numberButton.forEach((number) => {
   number.addEventListener('click', () => {
-    storedNumber += parseInt(number.textContent);
-    displayResult.textContent = storedNumber;
+    if(displayResult.textContent === '0') {
+      displayResult.textContent = '0';
+    } else if (firstNumber && clickedOperator) {
+      secondNumber += parseInt(number.textContent);
+      console.log('second', secondNumber);
+      displayResult.textContent = secondNumber;
+      console.log("DR", displayResult);
+    } else {
+      firstNumber += parseInt(number.textContent); 
+      console.log("FN", firstNumber);
+      displayResult.textContent = firstNumber;
+      console.log("elseDR", displayResult.textContent);
+    }
   })
 });
 operateButton.forEach((operator) => {
   operator.addEventListener('click', () => {
-        firstNumber = parseInt(displayResult.textContent);
-        clickedOperator = operator.textContent;
-        storedNumber = "";
+    if (clickedOperator && firstNumber && secondNumber) {
+      calculate(clickedOperator, firstNumber,secondNumber);
+      clickedOperator = operator.textContent;
+    } else {
+      clickedOperator = operator.textContent;
+    }
   })
 }); 
+
 equalsButton.addEventListener('click', () => {
-    if (clickedOperator && firstNumber && storedNumber) {
-      if (!parseFloat(storedNumber) && clickedOperator === '/') {
+    if (clickedOperator && firstNumber && secondNumber) {
+      if (!parseFloat(secondNumber) && clickedOperator === '/') {
         alert("Invalid dividend");
-        storedNumber = "";
+        secondNumber = "";
       } else {
-        result = operate(clickedOperator, parseFloat(firstNumber), parseFloat(storedNumber)).toFixed(2).replace(/[.,]00$/, "");
-        displayResult.textContent = result;
-        storedNumber = result;
+        calculate(clickedOperator, firstNumber,secondNumber);
       }
     }     
 });
 clearButton.addEventListener('click', () => {
   displayResult.textContent = "" ;
-  storedNumber="";
+  firstNumber="";
+  secondNumber="";
 });
 backButton.addEventListener('click', () => {  
     displayResult.textContent = displayResult.textContent.slice(0,-1);
-    storedNumber = storedNumber.slice(0,-1);
-});
-
-// window.addEventListener('keypress', (event) => {  
-//   if(
-//    event.key === '0' ||
-//    event.key === '1' ||
-//    event.key === '2' ||
-//    event.key === '3' ||
-//    event.key === '4' ||
-//    event.key === '5' ||
-//    event.key === '6' ||
-//    event.key === '7' ||
-//    event.key === '8' ||
-//    event.key === '9' 
-//   ){
-//     storedNumber += parseInt(event.key);
-//     displayResult.textContent = storedNumber;
-//   } else if (
-//     event.key === "+" ||
-//     event.key === "-" ||
-//     event.key === "*" ||
-//     event.key === "/"
-//   ){
-//     firstNumber = parseInt(event.key);
-//     clickedOperator = operateButton;
-//     storedNumber = "";
-//   } else if (event.key === "Enter") {
-//     if (clickedOperator && firstNumber && storedNumber) {
-//       if (!parseFloat(storedNumber) && clickedOperator === '/') {
-//         alert("Invalid dividend");
-//         storedNumber = "";
-//       } else {
-//         result = operate(clickedOperator, parseFloat(firstNumber), parseFloat(storedNumber)).toFixed(2).replace(/[.,]00$/, "");
-//         displayResult.textContent = result;
-//         storedNumber = result;
-//       }
-//     }    
-//   } else if (event.key === "Backspace") {
+    if (firstNumber && secondNumber) {
+      secondNumber = secondNumber.slice(0,-1);
+    } else if (firstNumber) {
+      firstNumber = firstNumber.slice(0,-1);
+    }
     
-//   }    
-// });
-  
+});
+decimalButton.addEventListener('click', () => {
+  if(operatorActive) {
+    displayResult.textContent = ('0.');
+    operatorActive = false;
+  } else if(displayResult.textContent.indexOf('.') === -1) {
+    displayResult.textContent = displayResult.textContent + '.';
+  }
+})
+window.addEventListener('keypress', (event) => {  
+  if(
+   event.key === '0' ||
+   event.key === '1' ||
+   event.key === '2' ||
+   event.key === '3' ||
+   event.key === '4' ||
+   event.key === '5' ||
+   event.key === '6' ||
+   event.key === '7' ||
+   event.key === '8' ||
+   event.key === '9' 
+  ){
+    if(displayResult.textContent === '0') {
+      displayResult.textContent = '0';
+    } else if (firstNumber && clickedOperator) {
+      secondNumber += parseInt(event.key);
+      console.log('second', secondNumber);
+      displayResult.textContent = secondNumber;
+      console.log("DR", displayResult);
+    } else {
+      firstNumber += parseInt(event.key); 
+      console.log("FN", firstNumber);
+      displayResult.textContent = firstNumber;
+      console.log("elseDR", displayResult.textContent);
+    }
+  }else if (
+  event.key === "+" ||
+  event.key === "-" ||
+  event.key === "*" ||
+  event.key === "/"
+  ){
+    if (clickedOperator && firstNumber && secondNumber) {
+      calculate(clickedOperator, firstNumber,secondNumber);
+      clickedOperator = event.key;
+    } else {
+      clickedOperator = event.key;
+    }
+  }else if (event.key === "Enter") {
+    if (clickedOperator && firstNumber && secondNumber) {
+      if (!parseFloat(secondNumber) && clickedOperator === '/') {
+        alert("Invalid dividend");
+        secondNumber = "";
+      } else {
+        calculate(clickedOperator, firstNumber,secondNumber);
+      }
+    }     
+  } else if (event.key === "Backspace") {
+    
+  }    
+});
